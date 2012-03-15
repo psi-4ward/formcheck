ce626a0b8afa21ab064c4128025c45a72304d6c2
Christoph Wiechert <wio@psitrax.de>
2012-03-15 17:58:14 +0100

    Fixed: Some german-translation improvements

--- plugins/formcheck/lang/de.js
+++ plugins/formcheck/lang/de.js
@@ -1,5 +1,5 @@
 formcheckLanguage = {
-	required: "Dieses Feld ist obligatorisch.",
+	required: "Dieses Feld muß ausgef&uuml;llt werden.",
 	alpha: "In diesem Feld sind nur Buchstaben zul&auml;ssig.",
 	alphanum: "In diesem Feld sind nur Zahlen zul&auml;ssig.",
 	nodigit: "Eingabe von Nummern nicht m&ouml;glich.",
@@ -14,7 +14,7 @@ formcheckLanguage = {
 	confirm: "Das Feld ist verschieden von %0.",
 	differs: "Der Wert muss unterschiedlich zu %0 sein.",
 	length_str: "Das Feld ist verschieden von %0.",
-	length_fix: "Falsche L�nge, es m�ssen exakt %0 Buchstaben sein.",
+	length_fix: "Falsche L&auml;nge, es m&uuml;ssen exakt %0 Buchstaben sein.",
 	lengthmax: "Der Wert ist nicht korrekt, maximale Anzahl Charakter %0.",
 	lengthmin: "Der Wert ist nicht korrekt, minimale Anzahl Charakter %0.",
 	checkbox: "Bitte aktivieren.",
